046c4c0b6e128208bf4018338cb6473c11f35684
JustPkDev <m.owais380o@gmail.com>
2025-03-17 08:00:04 +0500

    added links

--- template/contact.php
+++ template/contact.php
@@ -1,8 +1,8 @@
 <div class="bg-white rounded p-3">
     <h6>Links:</h6>
     <div class="mt-3">
-        <p class="m-0 mt-2">Website: <a href="#">JustPkDev</a></p>
-        <p class="m-0 mt-2">Plugin: <a href="#">Detail</a></p>
-        <p class="m-0 mt-2">Contact: <a href="#">Click</a></p>
+        <p class="m-0 mt-2">Website: <a href="https://justpkdev.vercel.app/">JustPkDev</a></p>
+        <p class="m-0 mt-2">Plugin: <a href="https://justpkdev.vercel.app/projects/27">Detail</a></p>
+        <p class="m-0 mt-2">Contact: <a href="https://justpkdev.vercel.app/?msg=1">Click</a></p>
     </div>
 </div>
